3b361e319141c281660ea23ad5347aab253968f7
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-04-02 15:37:18 +0200

    Update Player component to manage active state and notify parent of name changes

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,15 @@
 import {useState} from "react";
 
-export default function Player({initialName, symbol}) {
+export default function Player({initialName, symbol, isActive, onChangeName}) {
     const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
         setIsEditing((editing) => !editing);
+
+        if (isEditing) {
+            onChangeName(symbol, playerName);
+        }
     }
 
     function handleChange(event) {
@@ -14,14 +18,14 @@ export default function Player({initialName, symbol}) {
 
     let editablePlayerName = <span className='player-name'>{playerName}</span>;
 
-    if (!isEditing) {
+    if (isEditing) {
         editablePlayerName = (
-            <input type='text' required value={playerName} onChange={handleChange} />
+            <input type='text' required value={playerName} onChange={handleChange}/>
         );
     }
 
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
             <span className="player">
                 {editablePlayerName}
                 <span className='player-symbol'>{symbol}</span>
